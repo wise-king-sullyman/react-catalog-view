d39352875e9ebaa322a470ce6c06a209331ba9cb
Austin Sullivan <ausulliv@redhat.com>
2023-05-19 14:52:12 -0400

    test

--- release-config-test/release.config.js
+++ release-config-test/release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   branches: [
     { name: 'v4', channel: 'prerelease' },
-    { name: 'release-test-2', channel: 'alpha', prerelease: 'alpha' },
+    { name: 'refs/remotes/pull/4/merge', channel: 'alpha', prerelease: 'alpha' },
   ],
   analyzeCommits: {
     preset: 'angular'
